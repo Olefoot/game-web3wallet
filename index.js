14f652c3702de512cea359b4a897d31d00cff3ba
Olefoot <Olefoot.game@gmail.com>
2023-04-17 10:58:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function loadApp() {
     displayResponse("Follow the instructions for connecting to MetaMask:<br><br>1- Enter MetaMask with your username and password;<br>2- Select the account in Next;<br>3- Allow the site to connect;<br>4- Sign the transaction in Sign;<br><br>If you have any questions, please email<br>support@olecoin.io");
   } else {
     // Metamask não está instalado ou o objeto web3 não está disponível
-    displayResponse("MetaMask not installed in the browser!<br><br>Install the MetaMask plugin or copy the link below and open it in a browser with the plugin installed.<br>https://olefoot.github.io/game-web3wallet/?action=sign&message=1681648969<br><br>If you have questions about installation, click the button below and follow the instructions.<br>https://support.metamask.io/hc/en-us/articles/360015489531-Getting-started-with-MetaMask");
+    displayResponse("MetaMask not installed in the browser!<br><br>Install the MetaMask plugin or copy the link below and open it in a browser with the plugin installed.<br>" + window.location.href + "<br><br>If you have questions about installation, click the button below and follow the instructions.<br>https://support.metamask.io/hc/en-us/articles/360015489531-Getting-started-with-MetaMask");
   }
   //----
 
